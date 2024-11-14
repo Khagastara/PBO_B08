33d55d072e6ef106521013c9f24fc249bdd39c52
ArdiGantNK <ardiganteng4599@gmail.com>
2024-11-14 11:17:54 +0700

    p

--- PBO_B08/obj/Debug/net8.0-windows/PBO_B08.AssemblyInfo.cs
+++ PBO_B08/obj/Debug/net8.0-windows/PBO_B08.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PBO_B08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aeedb24caee154118830391f765416895f6ff33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c9e1838a37872b9c9d5a1cb7628b2016ea4488")]
 [assembly: System.Reflection.AssemblyProductAttribute("PBO_B08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PBO_B08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
