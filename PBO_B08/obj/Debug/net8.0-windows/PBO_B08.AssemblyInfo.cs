42ff0e4b5bdea8468d4a2b3279e6032c999bc1f9
Khagastara R. <fadhluaqil@gmail.com>
2024-12-11 12:55:19 +0700

    tes

--- PBO_B08/obj/Debug/net8.0-windows/PBO_B08.AssemblyInfo.cs
+++ PBO_B08/obj/Debug/net8.0-windows/PBO_B08.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PBO_B08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36fa015e9304aab4ae50a79f484d29d3a46106fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc826798c6348ff82fdd8f95da1dec53c4b674d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PBO_B08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PBO_B08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
